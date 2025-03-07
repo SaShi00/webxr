6286817f0efdaf8ba4c675ad192de47d1a4a1859
shinji mac <minimanberry@gmail.com>
2025-03-07 13:46:08 +0100

    change port for adapting render

--- src/server/server.js
+++ src/server/server.js
@@ -7,16 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173', // Allow requests from this origin
+        origin: '*', // Allow requests from any origin
         methods: ['GET', 'POST'],
     },
 });
 
-app.use(
-    cors({
-        origin: 'http://localhost:5173', // Allow requests from this origin
-    })
-);
+app.use(cors());
 
 // Dictionary to store avatar IDs and names
 const avatarDictionary = {};
@@ -104,6 +100,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
 });
